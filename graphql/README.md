95aa79314a675e4945573978d9f955b102fe1228
Marco Seabra <marcogutto@gmail.com>
2019-06-28 16:46:36 -0300

    Configuração do Express 'REST'

---
+++ graphql/README.md
@@ -0,0 +1,7 @@
+### SETUP
+
+#### WATCH BUILD
+npm run gulp
+
+#### START DEV
+npm run dev
---
+++ graphql/src/index.ts
@@ -0,0 +1,11 @@
+import * as http from 'http';
+
+import app from './app';
+import { normalizePort, onError, onListening } from './utils/utils';
+
+const server = http.createServer(app);
+const port = normalizePort(process.env.port || 3001);
+
+server.listen(port);
+server.on('error', onError(server));
+server.on('listening', onListening(server));
